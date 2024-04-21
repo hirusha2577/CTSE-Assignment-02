d5fd61407c7e3e7301bf2f8f650a6f5ca1f184da
Hirusha <56668123+hirusha2577@users.noreply.github.com>
2024-04-21 22:24:26 +0530

    change

--- product_service/eslint.config.mjs
+++ product_service/eslint.config.mjs
@@ -1,4 +1,5 @@
 import globals from "globals";
+
 import path from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,22 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
 
-export default {
-  languageOptions: {
-    ecmaVersion: 2021,  // This sets the ECMAScript version
-    sourceType: "module",  // Use "module" if your code uses ES6 modules
-    globals: {
-      ...globals.browser,  // Add browser-specific globals if needed
-      ...globals.node,  // Add Node.js-specific globals
-      __filename: "readonly",  // Define __filename as a global variable
-      __dirname: "readonly",  // Define __dirname as a global variable
-    }
-  },
-  plugins: [
-    ...compat.plugins,
-    "pluginJs"  // Assuming you have custom plugins or need to explicitly list them
-  ],
-  extends: [
-    ...compat.extends("standard-with-typescript")
-  ]
-};
+export default [
+  {languageOptions: { globals: globals.browser }},
+  ...compat.extends("standard-with-typescript"),
+];
