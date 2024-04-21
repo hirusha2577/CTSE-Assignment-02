7829149138b5c991a29c442d25e8ccbf860cf0da
Hirusha <56668123+hirusha2577@users.noreply.github.com>
2024-04-21 22:55:12 +0530

    change

--- product_service/eslint.config.mjs
+++ product_service/eslint.config.mjs
@@ -1,16 +1,15 @@
-import globals from 'globals';
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import pluginJs from '@eslint/js';
+import globals from 'globals'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended})
+const _FILENAME = fileURLToPath( import.meta.url )
+const _DRINAME = path.dirname( _FILENAME )
+const compat = new FlatCompat( { baseDirectory: _DRINAME, recommendedConfig: pluginJs.configs.recommended } )
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   ...compat.extends("standard-with-typescript")
 ]
